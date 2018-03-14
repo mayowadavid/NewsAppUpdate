58e9915d1a223823bdad6e53826b35ea19e4e8e3
Abhishek Kumar <abhishekint16@gmail.com>
2018-03-14 17:27:22 +0530

    implementation for retrofit

---
+++ app/src/main/java/abhishekint/com/newsappupdate/OkHttpModule.java
@@ -0,0 +1,27 @@
+package abhishekint.com.newsappupdate;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Named;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by abhishek on 14-03-2018.
+ */
+@Module
+public class OkHttpModule {
+
+    @Named("object_OkHttp")
+    @Provides
+    public OkHttpClient getOkHttp()
+    {
+        return new OkHttpClient.Builder()
+                .connectTimeout(30L, TimeUnit.SECONDS)
+                .readTimeout(30L, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
+                .build();
+    }
+}
--- app/src/main/java/abhishekint/com/newsappupdate/RetrofitInitModule.java
+++ app/src/main/java/abhishekint/com/newsappupdate/RetrofitInitModule.java
@@ -1,11 +1,28 @@
 package abhishekint.com.newsappupdate;
 
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Named;
+
 import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by abhishek on 14-03-2018.
  */
 
-@Module
+@Module(includes = OkHttpModule.class)
 public class RetrofitInitModule {
+
+    @Provides
+    public Retrofit getClient(String baseURL,@Named("object_OkHttp") OkHttpClient okHttpClient) {
+        return new Retrofit.Builder()
+                .baseUrl(baseURL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
 }
