9608c15c419a6f16adeacb3b882260a7a56c9b5f
Abhishek Kumar <abhishekint16@gmail.com>
2018-03-14 15:04:53 +0530

    adding annotationProcessor for genrating source code out of annotations and creating application level class to genrate dagger class

---
+++ app/src/main/java/abhishekint/com/newsappupdate/NewsApplication.java
@@ -0,0 +1,17 @@
+package abhishekint.com.newsappupdate;
+
+import android.app.Application;
+
+/**
+ * Created by abhishek on 14-03-2018.
+ */
+
+public class NewsApplication extends Application {
+
+    DataManagerInterface dataManagerInterface;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        dataManagerInterface= DaggerDataManagerInterface.builder().build();
+    }
+}
