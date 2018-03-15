a92d56a136e7129aaf1f20fc5dfdc37c10b8fc96
Abhishek Kumar <abhishekint16@gmail.com>
2018-03-15 12:30:46 +0530

    simple butterknife

--- app/src/main/java/abhishekint/com/newsappupdate/MainActivity.java
+++ app/src/main/java/abhishekint/com/newsappupdate/MainActivity.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import javax.inject.Inject;
 
+import butterknife.BindColor;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import retrofit2.Retrofit;
 
 /**
@@ -17,10 +23,26 @@ public class MainActivity extends AppCompatActivity {
 
     @Inject
     public Retrofit retrofit;
+    @BindView(R.id.activity_main_tv)
+    TextView activity_main_tv;
+    Unbinder unbinder;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((NewsApplication) getApplicationContext()).getNewsAppComponent().b(this);
+        unbinder=ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.activity_main_tv)
+    public void buttonClick()
+    {
+        activity_main_tv.setText("Awesome Life!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }
