d1d4f1aab22178ad0e875062dd5ba80df033a296
Abhishek Kumar <abhishekint16@gmail.com>
2018-03-17 15:26:04 +0530

    creating news fragment

---
+++ app/src/main/java/abhishekint/com/newsappupdate/app/MainActivity/NewsFragment/NewsFragment.java
@@ -0,0 +1,16 @@
+package abhishekint.com.newsappupdate.app.MainActivity.NewsFragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by abhishek on 17-03-2018.
+ */
+
+public class NewsFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
